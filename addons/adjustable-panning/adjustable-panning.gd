fd2af3facc93a99ab3596a0122211767a2a42857
Legendgreat <syftelurth@gmail.com>
2026-09-24 10:16:39 +0900

    Refactor viewport input handling and cleanup,
    Refactor input handling to improve viewport interaction and streamline event processing.

--- addons/adjustable-panning/adjustable-panning.gd
+++ addons/adjustable-panning/adjustable-panning.gd
@@ -6,26 +6,44 @@ const XBUTTON_2_SETTING := "adjustable_panning/mouse_button_5"
 
 const PLUGIN_NAME := "Adjustable Panning"
 
-var canvas_viewport: Control
+const CLASS_NAMES := [
+	"CanvasItemEditorViewport", 
+	"TileAtlasView", 
+	"GenericTilePolygonEditor", 
+]
+
+var viewport_dict: Dictionary[Control, String]
+var viewport_callbacks: Dictionary[Control, Callable]
+
 var config_dialog: AcceptDialog
 var xbutton1_check_box: CheckBox
 var xbutton2_check_box: CheckBox
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var mouse_button := event as InputEventMouseButton
+
+		if mouse_button.button_index == MOUSE_BUTTON_MIDDLE:
+			print(
+					"PLUGIN INPUT MB3: ",
+					mouse_button.position
+			)
+
 func _enter_tree() -> void:
-	_connect_canvas_input()
+	_connect_viewport_inputs()
 	_add_tool_submenu()
 	_setup_config_menu()
 
 func _exit_tree() -> void:
-	_disconnect_canvas_input()
+	_disconnect_viewport_input()
 	_remove_tool_submenu()
 	_cleanup_config_menu()
-	
-	canvas_viewport = null
 
-func _on_canvas_gui_input(event: InputEvent) -> void:
+func _on_viewport_gui_input(event: InputEvent, viewport: Control) -> void:
 	if not event is InputEventMouseButton:
 		return
+	
+	print(viewport.get_class(), ": ", event)
 
 	if not _is_configured_button(event.button_index):
 		return
@@ -36,8 +54,7 @@ func _on_canvas_gui_input(event: InputEvent) -> void:
 	middle_button.position = event.position
 	middle_button.global_position = event.global_position
 
-	canvas_viewport.gui_input.emit(middle_button)
-
+	viewport.get_viewport().push_input(middle_button)
 
 func _is_configured_button(button: MouseButton) -> bool:
 	var settings := EditorInterface.get_editor_settings()
@@ -52,18 +69,31 @@ func _is_configured_button(button: MouseButton) -> bool:
 		_:
 			return false
 
-func _connect_canvas_input() -> void:
-	canvas_viewport = _find_control_by_class(
+func _connect_viewport_inputs() -> void:
+	for c in CLASS_NAMES:
+		var viewports := _find_control_by_class(
 			EditorInterface.get_base_control(),
-			"CanvasItemEditorViewport"
-	)
+			c
+		)
+		
+		for viewport in viewports:
+			var callback := _on_viewport_gui_input.bind(viewport)
+			viewport.gui_input.connect(callback)
+			
+			viewport_dict[viewport] = c
+			viewport_callbacks[viewport] = callback
+		
 
-	if canvas_viewport:
-		canvas_viewport.gui_input.connect(_on_canvas_gui_input)
-
-func _disconnect_canvas_input() -> void:
-	if canvas_viewport:
-		canvas_viewport.gui_input.disconnect(_on_canvas_gui_input)
+func _disconnect_viewport_input() -> void:
+	for viewport in viewport_dict:
+		var callback: Callable = viewport_callbacks[viewport]
+		
+		if is_instance_valid(viewport):
+			viewport.gui_input.disconnect(callback)
+	
+	viewport_dict.clear()
+	viewport_callbacks.clear()
+	
 		
 func _add_tool_submenu() -> void:
 	var tool_menu: PopupMenu = PopupMenu.new()
@@ -128,6 +158,8 @@ func _open_config() -> void:
 	config_dialog.canceled.connect(_cleanup_config_menu)
 	
 	config_dialog.popup_centered()
+	
+	print(viewport_dict)
 
 func _save_settings() -> void:
 	EditorInterface.get_editor_settings().set_setting(
@@ -149,16 +181,26 @@ func _cleanup_config_menu() -> void:
 	config_dialog = null
 	xbutton1_check_box = null
 	xbutton2_check_box = null
-	
 
-func _find_control_by_class(node: Node, _class_name: String) -> Control:
+func _find_control_by_class(node: Node, _class_name: String) -> Array[Control]:
+	var results: Array[Control] = []
+	
 	if node is Control and node.get_class() == _class_name:
-		return node
+		results.append(node)
 	
 	for child in node.get_children():
-		var result := _find_control_by_class(child, _class_name)
+		results.append_array(_find_control_by_class(child, _class_name))
+	
+	return results
 
-		if result:
-			return result
+func _dump_controls(node: Node, indent := 0) -> void:
+	if node is Control:
+		print(
+				"  ".repeat(indent),
+				node.get_class(),
+				" | ",
+				node.name
+		)
 
-	return null
+	for child in node.get_children():
+		_dump_controls(child, indent + 1)
